8045ad45c38dfe625e8d92f1d1f77271c79e7ff8
reyreavman <radmirkhurum@gmail.com>
2024-08-04 21:02:48 +0300

    написан ридер

---
+++ src/main/java/ru/rrk/readers/InputFileReader.java
@@ -0,0 +1,26 @@
+package ru.rrk.readers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class InputFileReader {
+    public List<String> readFile(File file) {
+        ArrayList<String> values = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while (true) {
+                String string = reader.readLine();
+                if (string == null) break;
+                else values.add(string);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(100);
+        }
+        return Collections.unmodifiableList(values);
+    }
+}
