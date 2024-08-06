085e076c2ee9f4f0cf9c5100acc53d1d4bd26f2b
reyreavman <radmirkhurum@gmail.com>
2024-08-06 23:20:11 +0300

    обновление импортов класса

--- src/main/java/ru/rrk/Main.java
+++ src/main/java/ru/rrk/Main.java
@@ -2,24 +2,26 @@ package ru.rrk;
 
 import com.beust.jcommander.JCommander;
 import ru.rrk.args.Args;
-import ru.rrk.args.ArgsParser;
+import ru.rrk.args.ArgsHandler;
 import ru.rrk.classifiers.DataTypeClassifier;
 import ru.rrk.readers.InputFileReader;
 import ru.rrk.writers.factory.WriterFactory;
 
 public class Main {
     public static void main(String[] appArgs) {
+        long startTime = System.currentTimeMillis();
         Args args = new Args();
         JCommander commander = JCommander.newBuilder()
                 .addObject(args)
                 .programName("JavaTestTask")
                 .allowParameterOverwriting(false)
                 .build();
-        ArgsParser.parse(args, commander, appArgs);
+        ArgsHandler.parse(args, commander, appArgs);
         InputFileReader reader = new InputFileReader();
         DataTypeClassifier classifier = new DataTypeClassifier("^-?\\d+$", "^[-+]?[0-9]*[.,][0-9]+(?:[eE][-+]?[0-9]+)?$");
         WriterFactory writerFactory = new WriterFactory();
         Manager manager = new Manager(args, reader, classifier, writerFactory).start();
         System.out.println(args.toString());
+        System.out.printf("time - %d%n", System.currentTimeMillis() - startTime);
     }
 }
--- src/main/java/ru/rrk/writers/factory/WriterFactory.java
+++ src/main/java/ru/rrk/writers/factory/WriterFactory.java
@@ -1,12 +1,12 @@
 package ru.rrk.writers.factory;
 
 import ru.rrk.writers.Writer;
-import ru.rrk.writers.WriterImpl;
+import ru.rrk.writers.DefaultWriter;
 
 import java.io.BufferedWriter;
 
 public class WriterFactory {
     public Writer<String> createWriter(BufferedWriter writer) {
-        return new WriterImpl(writer);
+        return new DefaultWriter(writer);
     }
 }
